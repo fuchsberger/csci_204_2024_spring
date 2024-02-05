724fab0216f0abe5e304648df19c09573ad7d1f0
Alexander Fuchsberger <8102991+fuchsberger@users.noreply.github.com>
2024-02-05 10:19:41 -0500

    added lecture 4.1 notes

---
+++ week_4/test_stack.py
@@ -0,0 +1,29 @@
+import unittest
+
+from stack import Stack, StackError #, LimitedStack
+
+class StackTests(unittest.TestCase):
+    def setUp(self):
+        self.stack = Stack()
+
+    def test_length(self):
+        self.assertEqual(len(self.stack), 0)
+        self.stack.push(1)
+        self.assertEqual(len(self.stack), 1)
+        self.stack.push(0)
+        self.assertEqual(len(self.stack), 0)
+
+    def test_order(self):
+        self.stack.push(1)
+        self.stack.push(2)
+        self.stack.push(3)
+
+        self.assertEqual(self.stack.pop(), 3)
+        self.assertEqual(self.stack.pop(), 2)
+        self.assertEqual(self.stack.pop(), 1)
+
+        with self.assertRaises(StackError):
+            self.stack.pop()
+
+    def test_limited_stack(self):
+

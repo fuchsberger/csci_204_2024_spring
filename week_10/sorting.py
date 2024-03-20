02a6bdab78493d7ecf37cc01b69d7ee007d81fe6
Alexander Fuchsberger <8102991+fuchsberger@users.noreply.github.com>
2024-03-20 10:52:44 -0400

    added lecture 10.2 notes

--- week_10/sorting.py
+++ week_10/sorting.py
@@ -1,7 +1,16 @@
 def selection_sort(sequence):
     # TODO: Activity 1
+    # loop through all indexes (excluding last)
+        # keep track of current index
+        # loop through all index between current and last
+            # find smallest in remaining elements
+        # swap if found smallest is smaller than then current
     return sequence
 
 def insertion_sort(sequence):
     # TODO: Activity 2
+
+    # loop through all indexes
+    # keep track of current index and next element
+      # shift back element until no longer smaller than then current
     return sequence
